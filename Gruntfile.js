b2ef7e551c00f4659447c222a047e702070d2abf
Natalie Akam <natalie.akam@gmail.com>
2015-11-26 00:03:11 +0000

    Added testing dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,23 @@ module.exports = function(grunt){
     watch: {
       files: ['Gruntfile.js'],
       tasks: ['jshint']
-    }
+    },
+    webdriver: {
+      helloWorld: {
+        tests: 'test/**/*'
+      },
+      options: {
+        desiredCapabilities: {
+          browserName: 'chrome'
+        }
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-webdriver');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'webdriver']);
 
 };
